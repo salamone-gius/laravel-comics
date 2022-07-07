957cb9d0592267d6cd4f7342050d055a6b0e549d
salamone-gius <salamone.gius@gmail.com>
2022-07-07 17:55:32 +0200

    header wip

---
+++ config/links.php
@@ -0,0 +1,55 @@
+<?php
+
+    return [
+        [
+            "text" => "characters",
+            "url" => "#",
+            "current" => false,
+        ],
+        [
+            "text" => "comics",
+            "url" => "#",
+            "current" => true,
+        ],
+        [
+            "text" => "movies",
+            "url" => "#",
+            "current" => false,
+        ],
+        [
+            "text" => "tv",
+            "url" => "#",
+            "current" => false,
+        ],
+        [
+            "text" => "games",
+            "url" => "#",
+            "current" => false,
+        ],
+        [
+            "text" => "collectibles",
+            "url" => "#",
+            "current" => false,
+        ],
+        [
+            "text" => "videos",
+            "url" => "#",
+            "current" => false,
+        ],
+        [
+            "text" => "fans",
+            "url" => "#",
+            "current" => false,
+        ],
+        [
+            "text" => "news",
+            "url" => "#",
+            "current" => false,
+        ],
+        [
+            "text" => "shop",
+            "url" => "#",
+            "current" => false,
+        ],
+    ]
+?>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,28 @@
+<header>
+    <div class="header-top">
+        <div class="container">
+            <ul>
+                <li>
+                    <a href="#">dc power visa</a>
+                    <i class="fa-regular fa-registered reg"></i>
+                </li>
+                <li>
+                    <a href="#">additional dc sites</a>
+                    <i class="fa-solid fa-caret-down caret"></i>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <div class="header-bottom container">
+        <div class="header__logo">
+            <img src="{{asset('img/dc-logo.png')}}" alt="DC-Logo">
+        </div>
+        <nav class="header__nav">
+            <ul>
+                {{-- <li v-for="(link, index) in links" :key="index">
+                    <a :class="{active : link.current}" :href="link.url">{{link.text}}</a>
+                </li> --}}
+            </ul>
+        </nav>
+    </div>
+</header>
