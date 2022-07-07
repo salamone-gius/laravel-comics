2483f9afc6ec7c39f5d281d57ccd1e0060c6cd19
salamone-gius <salamone.gius@gmail.com>
2022-07-07 15:47:29 +0200

    base layout created

---
+++ resources/views/layouts/base.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>@yield('page-title', 'DC Comics')</title>
+    </head>
+    <body>
+        @include('partials.header')
+        <main>
+            @yield('page-content')
+        </main>
+        @include('partials.footer')
+    </body>
+</html>
