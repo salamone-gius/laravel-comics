dc05e2553d449ef0272ef360d26bc61474cb4198
salamone-gius <salamone.gius@gmail.com>
2022-07-08 14:46:04 +0200

    footer-bottom created and footer completed

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -101,5 +101,39 @@
             </div>
         </div>
     </div>
-    <div class="footer-bottom"></div>
+    <div class="footer-bottom">
+        <div class="container">
+            <button class="footer-bt button">sign-up now!</button>
+            <nav class="footer-nav">
+                <span>follow us</span>
+                <ul>
+                    <li>
+                        <a href="#">
+                            <img src="{{asset('img/footer-facebook.png')}}" alt="facebook">
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <img src="{{asset('img/footer-twitter.png')}}" alt="twitter">
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <img src="{{asset('img/footer-youtube.png')}}" alt="youtube">
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <img src="{{asset('img/footer-pinterest.png')}}" alt="pinterest">
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <img src="{{asset('img/footer-periscope.png')}}" alt="periscope">
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </div>
 </footer>
