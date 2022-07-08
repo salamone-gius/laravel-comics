fc919411f27ed3f314cb7ba7b3831a30947fbf79
salamone-gius <salamone.gius@gmail.com>
2022-07-08 14:29:03 +0200

    footer-top created

--- resources/views/main-sections/navigation.blade.php
+++ resources/views/main-sections/navigation.blade.php
@@ -2,31 +2,31 @@
     <nav class="container">
         <ul>
           <li>
-            <a>
+            <a href="#">
               <img src="{{asset('img/buy-comics-digital-comics.png')}}" alt="Digital Comics">
               <span>digital comics</span>
             </a>
           </li>
           <li>
-            <a>
+            <a href="#">
               <img src="{{asset('img/buy-comics-merchandise.png')}}" alt="DC Merchandise">
               <span>dc merchandise</span>
             </a>
           </li>
           <li>
-            <a>
+            <a href="#">
               <img src="{{asset('img/buy-comics-subscriptions.png')}}" alt="Subscription">
               <span>subscription</span>
             </a>
           </li>
           <li>
-            <a>
+            <a href="#">
               <img class="other" src="{{asset('img/buy-comics-shop-locator.png')}}" alt="Comic Shop Locator">
               <span>comic shop locator</span>
             </a>
           </li>
           <li>
-            <a>
+            <a href="#">
               <img src="{{asset('img/buy-dc-power-visa.svg')}}" alt="DC Power Visa">
               <span>dc power visa</span>
             </a>
---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,105 @@
+<footer>
+    <div class="footer-top">
+        <div class="container">
+            <div class="footer-top-left">
+                <div class="col left">
+                    <ul>
+                        <p>DC COMICS</p>
+                        <li>
+                            <a href="#">Characters</a>
+                        </li>
+                        <li>
+                            <a href="#">Comics</a>
+                        </li>
+                        <li>
+                            <a href="#">Movies</a>
+                        </li>
+                        <li>
+                            <a href="#">Tv</a>
+                        </li>
+                        <li>
+                            <a href="#">Games</a>
+                        </li>
+                        <li>
+                            <a href="#">Videos</a>
+                        </li>
+                        <li>
+                            <a href="#">News</a>
+                        </li>
+                    </ul>
+                    <ul>
+                        <p>SHOP</p>
+                        <li>
+                            <a href="#">Shop DC</a>
+                        </li>
+                        <li>
+                            <a href="#">Shop DC Collectibles</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col middle">
+                    <ul>
+                        <p>DC</p>
+                        <li>
+                            <a href="#">Terms Of Use</a>
+                        </li>
+                        <li>
+                            <a href="#">Privacy Policy (New)</a>
+                        </li>
+                        <li>
+                            <a href="#">Ad Choices</a>
+                        </li>
+                        <li>
+                            <a href="#">Advertising</a>
+                        </li>
+                        <li>
+                            <a href="#">Jobs</a>
+                        </li>
+                        <li>
+                            <a href="#">Subscriptions</a>
+                        </li>
+                        <li>
+                            <a href="#">Talent Workshops</a>
+                        </li>
+                        <li>
+                            <a href="#">CPSC Certificates</a>
+                        </li>
+                        <li>
+                            <a href="#">Ratings</a>
+                        </li>
+                        <li>
+                            <a href="#">Shop Help</a>
+                        </li>
+                        <li>
+                            <a href="#">Contact us</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col right">
+                    <ul>
+                        <p>SITES</p>
+                        <li>
+                            <a href="#">DC</a>
+                        </li>
+                        <li>
+                            <a href="#">MAD Magazine</a>
+                        </li>
+                        <li>
+                            <a href="#">DC Kids</a>
+                        </li>
+                        <li>
+                            <a href="#">DC Universe</a>
+                        </li>
+                        <li>
+                            <a href="#">DC Power Visa</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="footer-top-right">
+                <img src="{{asset('img/dc-logo-bg.png')}}" alt="dc-logo-bg">
+            </div>
+        </div>
+    </div>
+    <div class="footer-bottom"></div>
+</footer>
