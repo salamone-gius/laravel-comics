ba40dfe8e9fd1bdaf465c00a1248ce49b9bcbf89
salamone-gius <salamone.gius@gmail.com>
2022-07-08 13:38:24 +0200

    sectionNavigation created and added to main

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,4 +7,5 @@
 @section('page-content')
     @include('main-sections.hero')
     @include('main-sections.comics')
+    @include('main-sections.navigation')
 @endsection
---
+++ resources/views/main-sections/navigation.blade.php
@@ -0,0 +1,36 @@
+<div class="navigation-section">
+    <nav class="container">
+        <ul>
+          <li>
+            <a>
+              <img src="{{asset('img/buy-comics-digital-comics.png')}}" alt="Digital Comics">
+              <span>digital comics</span>
+            </a>
+          </li>
+          <li>
+            <a>
+              <img src="{{asset('img/buy-comics-merchandise.png')}}" alt="DC Merchandise">
+              <span>dc merchandise</span>
+            </a>
+          </li>
+          <li>
+            <a>
+              <img src="{{asset('img/buy-comics-subscriptions.png')}}" alt="Subscription">
+              <span>subscription</span>
+            </a>
+          </li>
+          <li>
+            <a>
+              <img class="other" src="{{asset('img/buy-comics-shop-locator.png')}}" alt="Comic Shop Locator">
+              <span>comic shop locator</span>
+            </a>
+          </li>
+          <li>
+            <a>
+              <img src="{{asset('img/buy-dc-power-visa.svg')}}" alt="DC Power Visa">
+              <span>dc power visa</span>
+            </a>
+          </li>
+        </ul>
+      </nav>
+</div>
